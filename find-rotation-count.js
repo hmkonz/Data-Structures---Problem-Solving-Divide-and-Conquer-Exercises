28ff82c01930c0a6930dc91d3db36e95178f4af1
Heather Konz <hmkonz@yahoo.com>
2023-11-27 17:57:33 -0700

    Modified comments

--- find-rotation-count.js
+++ find-rotation-count.js
@@ -1,6 +1,6 @@
 // find the number of times the array has been rotated counter-clockwise
 function findRotationCount(array) {
-  // if 'array' only has one element OR the first element in 'array' < the last element in 'array', return 0 as the pivot (first element in the array is the pivot)
+  // if 'array' only has one element OR the first element in 'array' < the last element in 'array', return 0 as the number of times the array has been rotated (no rotation can/did occur)
   if (array.length === 1 || array[0] < array[array.length - 1]) return 0;
   // assign the index of the first element in 'array'
   let leftIdx = 0;
@@ -14,7 +14,7 @@ function findRotationCount(array) {
     // assign the value of the element in the middle of 'array'
     let middleVal = array[middleIdx];
 
-    // if 'middleVal' (the value of the element in the middle of the array) > 'middleVal +1' (the value of the element in 'array' to the right of 'middleVal'), return a pivot of 'middleIdx +1' (pivot is the number to the right of middleVal. i.e for array=[6,7,8,1,2,3] and num=8, middleIdx=2 and middleVal=8. Since middleVal > middleVal + 1 (8 > 1) then the pivot = index of 1 (middleVal + 1))
+    // if 'middleVal' (the value of the element in the middle of the array) > 'middleVal +1' (the value of the element in 'array' to the right of 'middleVal'), return number of times the array has been rotated as 'middleIdx +1' (i.e for array=[6,7,8,1,2,3] and num=8, middleIdx=2 and middleVal=8. Since middleVal > middleVal + 1 (8 > 1) then the number of times the array has been rotated = index of 1 (middleVal + 1))
     if (middleVal > array[middleIdx + 1]) return middleIdx + 1;
     // if the value of the element in 'array' at 'leftIdx' <= 'middleVal' (the value of the element at 'middleIdx') then assign 'leftIdx' to 'middleIdx + 1' (now will only look at the right side of 'array' for the pivot)
     else if (array[leftIdx] <= middleVal) {
